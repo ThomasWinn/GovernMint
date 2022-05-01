5dbb153061660ac293ec908296af5cf96ad79479
Calvin Tran <tran0728@umn.edu>
2022-05-01 17:52:51 -0500

    summary stuff

--- src/views/SummaryView.vue
+++ src/views/SummaryView.vue
@@ -6,19 +6,20 @@
             <option value="Bar">Bar</option>
             <option value="Radar">Radar</option>
         </select>
-        <!-- <BarChart /> -->
+
     <RadarChart v-if = "loaded && loadRadar" :chart-data="chartData" :chart-options="chartOptions" />
     <BarChart v-if = "loaded && loadBar" :chart-data="chartData" :chart-options="chartOptions" />
     <PieChart v-if = "loaded && loadPie" :chart-data="chartData" :chart-options="chartOptions" />
 
-        <!-- <PolarArea v-if = "loaded" :chart-data="chartData" :chart-options="chartOptions" /> -->
     <h1 class="uk-heading-bullet"> Key Summary </h1>
     <ul class="uk-list uk-list-divider">
-        <li>what</li>
+        <li>Your top 3 spending categories are {{sorted_categories[0][0]}}, {{sorted_categories[1][0]}}, and {{sorted_categories[2][0]}}</li>
+        <li v-if ="budget_percent != 0">You have spent {{budget_percent}}% of your budget.</li>
+        <li>Your most used payment type is {{sorted_payTypes[0][0]}} </li>
     </ul>
     </div>
-    
 </template>
+
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
@@ -40,8 +41,12 @@ export default {
         loadPie: true,
         loadBar: false,
         loadRadar: false,
+        profile: null,
+        budget_percent: 0,
         chartData: { labels:["dog"], datasets: ["3"] },
         owner_transactions: null,
+        sorted_categories: null,
+        sorted_payTypes: null,
         chartOptions: {
             responsive: true,
             maintainAspectRatio: false,
@@ -52,8 +57,63 @@ export default {
                  }
             }
         }
-        
     }),
+ 
+    watch: {
+        owner_transactions(new_transaction, old_transaction) {
+            this.loaded = false
+            var category_totals = {}
+            console.log(this.owner_transactions.length)
+            for(let i = 0; i < this.owner_transactions.length; i++) {
+                if(!Object.prototype.hasOwnProperty.call(category_totals,this.owner_transactions[i].category)) {
+                    category_totals[this.owner_transactions[i].category] = this.owner_transactions[i].amount
+                }   
+                else {
+                    category_totals[this.owner_transactions[i].category] += this.owner_transactions[i].amount
+                }
+            }
+            this.chartData = {labels: Object.keys(category_totals), datasets: [{data: Object.values(category_totals), backgroundColor: ['lime', '#41B883', '#E46651', '#00D8FF', '#DD1B16',"rgba(255,99,132,1)" ], fill: true, label: "User summary", }]}
+            this.loaded = true
+            var categories = Object.keys(category_totals).map(function(key) {
+                return [key, category_totals[key]]
+            })
+            categories.sort(function(first,second) {
+                return second[1] - first[1];
+            })
+            this.sorted_categories = categories
+
+
+            var payment_frequencies = {}
+            for(let i = 0; i < this.owner_transactions.length; i++) {
+                if(!Object.prototype.hasOwnProperty.call(payment_frequencies,this.owner_transactions[i].paymentType)) {
+                    payment_frequencies[this.owner_transactions[i].paymentType] = 0
+                }   
+                else {
+                    payment_frequencies[this.owner_transactions[i].paymentType] += 1
+                }
+            }
+            console.log(payment_frequencies)
+            var paymentTypes = Object.keys(payment_frequencies).map(function(key) {
+                return [key, payment_frequencies[key]]
+            })
+            paymentTypes.sort(function(first,second) {
+                return second[1] - first[1];
+            })
+            this.sorted_payTypes = paymentTypes
+
+
+        },
+        profile(new_profile, old_profile) {
+            var spent = 0;
+            this.owner_transactions.forEach(e => {
+                spent += e.amount;
+            });
+            console.log("here")
+            console.log(this.profile[0].balance)
+            this.budget_percent = spent / this.profile[0].balance * 100
+            console.log(this.budget_percent)
+        }
+    },
     methods: {
         onChange(event) {
             if(event.target.value == "Pie") {
@@ -105,43 +165,13 @@ export default {
                 this.loadBar = false
                 this.loadRadar = true
             }
-
-
-        },
-
-    },
-    watch: {
-        owner_transactions(new_transaction, old_transaction) {
-            this.loaded = false
-            var category_totals = {}
-            console.log(this.owner_transactions)
-            console.log(this.owner_transactions.length)
-            for(let i = 0; i < this.owner_transactions.length; i++) {
-                if(!Object.prototype.hasOwnProperty.call(category_totals,this.owner_transactions[i].category)) {
-                    category_totals[this.owner_transactions[i].category] = this.owner_transactions[i].amount
-                }   
-                else {
-                    category_totals[this.owner_transactions[i].category] += this.owner_transactions[i].amount
-                }
-            }
-            this.chartData = {labels: Object.keys(category_totals), datasets: [{data: Object.values(category_totals), backgroundColor: ['lime', 'blue', 'yellow','aqua','orange',"rgba(255,99,132,1)" ], fill: true, label: "User summary", }]}
-            console.log(this.chartData)
-            this.loaded = true
-            categories = Object.keys(category_totals)
-            var len = categories.length
-            categories.sort()
-            var sortedCategories = [];
-            for (let i = 0 ; i < len; i++) {
-                let k = keys[i]
-                sortedCategories.push({'key': k, 'value': dict[k]})
-            }
-            console.log(sortedCategories)
         },
     },
     firestore: function() {        
         return {
             spendingCategories: db.collection("spendingCategories").where('user', '==', auth.currentUser.uid),
             owner_transactions: db.collection('transactions').where('owner', '==', auth.currentUser.uid),
+            profile: db.collection('profile').where('owner', '==', auth.currentUser.uid)
        
         }
     }
